b17a04620972f886bdc9a4a716ca8e826117da65
Your Name <you@example.com>
2023-11-16 17:11:51 +0200

    Initial project creation

---
+++ AppiumSummatorTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace AppiumSummatorTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
